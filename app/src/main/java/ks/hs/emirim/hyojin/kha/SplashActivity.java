27e74c3283a0c6d12bcfce8906c3d81f626fe96c
User <w2030@e-mirim.hs.kr>
2021-07-26 15:02:14 +0900

    Add SplashActivity.java(Edit) Commit

---
+++ app/src/main/java/ks/hs/emirim/hyojin/kha/SplashActivity.java
@@ -0,0 +1,22 @@
+package ks.hs.emirim.hyojin.kha;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try{
+            Thread.sleep(4000); // 대기 초 설정
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
+    }
+}
